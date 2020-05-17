86f2957c93d1ceba1f0e08549651898e518ff39b
itdboy <itdboy@gmail.com>
2020-05-17 15:14:40 +0700

    office

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import React from 'react';
- 
+
 // import Cards from './components/Cards/Cards';
 // import Chart from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker';  
 
-import {Cards, Chart, CountryPicker} from './components'
+import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css';
-import {fetchData} from './api';
+import { fetchData } from './api';
+import coronaImage from './images/image.png';
 
-class App extends React.Component{
+class App extends React.Component {
 
   state = {
-    data:{},
+    data: {},
+    country: '',
   }
 
-  async componentDidMount(){
- 
+  async componentDidMount() {
+
     const fecthedData = await fetchData();
 
- 
-    this.setState({ data:fecthedData});
- 
+
+    this.setState({ data: fecthedData });
+
   }
 
-  render(){
-      const {data} = this.state;
-      return (
-          <div className={styles.container}>
-             
-             <Cards data={data}/>
-             <Chart/>
-             <CountryPicker/>
-          </div>
-      )
+  handleCountryChange = async (country) => {
+
+    const fecthedData = await fetchData(country);
+
+    this.setState({ data: fecthedData, country: country });
+
+  }
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <Cards data={data} />
+
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    )
   }
 }
 
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,38 +1,79 @@
-import React, {useState, useEffect} from 'react' ;
+import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
-import { Line, Bar} from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart =() => {
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
 
-    const [dailyData, setDailyData] = useState({});
- 
+    const [dailyData, setDailyData] = useState([]);
 
-    useEffect(()=>{
-        const  fetchAPI = async () => {
-           setDailyData(await fetchDailyData()); 
-        }       
-        
-        console.log(dailyData);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
 
         fetchAPI();
-        
-    }, [dailyData] );
+
+    }, []);
+
 
     const lineChart = (
-        dailyData[0]
-        ?(
-        <Line
-            data={{
-                labels:'',
-                datasets: [{}, {}],
-            }}
-        />):null
+        dailyData.length
+            ? (
+                <Line
+                    data={{
+                        labels: dailyData.map(({ date }) => date),
+                        datasets: [{
+                            data: dailyData.map(({ confirmed }) => confirmed),
+                            label: 'Infected',
+                            borderColor: '#333ff',
+                            fill: true,
+                        }, {
+                            data: dailyData.map(({ deaths }) => deaths),
+                            label: 'Deaths',
+                            borderColor: 'red',
+                            backgroundColor: 'rgba(255,0,0,0.5)',
+                            fill: true,
+                        }],
+                    }}
+                />) : null
     );
-   
+
+    console.log(confirmed, recovered, deaths);
+
+    const barChart = (
+        confirmed
+            ? (
+                <Bar
+                    data={{
+                        labels: ['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: [
+                                'rgba(0,0,255,0.5)',
+                                'rgba(0,255,0,0.5)',
+                                'rgba(255,0,0.5)',
+                            ],
+                            data: [confirmed.value, recovered.value, deaths.value]
+                        }]
+                    }}
+                    options={{
+                        legend: { display: false },
+                        title: { display: true, text: `Current state in ${country}` },
+                    }}
+
+                />
+            ) : null
+    )
+
+
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {country ? barChart : lineChart}
+        </div>
     )
 }
 
