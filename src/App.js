7b5c5e3ceb3c62ca4b2e5f034a786e2a61cd286a
itdboy <itdboy@gmail.com>
2020-05-17 09:57:34 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
  
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker';  
 
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css';
+import {fetchData} from './api';
 
 class App extends React.Component{
+
+  state = {
+    data:{},
+  }
+
+  async componentDidMount(){
+ 
+    const fecthedData = await fetchData();
+
+ 
+    this.setState({ data:fecthedData});
+ 
+  }
+
   render(){
+      const {data} = this.state;
       return (
-          <div>
-              <h1>App</h1>
+          <div className={styles.container}>
+             
+             <Cards data={data}/>
+             <Chart/>
+             <CountryPicker/>
           </div>
       )
   }
